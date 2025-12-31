2ea02ed0cb4276be71ce7794e3e838178aca6dcb
Matt Williams <matt@neverstew.com>
2025-12-31 11:21:39 +0000

    Add area

--- src/baseline/onSiteHabitat.ts
+++ src/baseline/onSiteHabitat.ts
@@ -1,11 +1,23 @@
-import type { BroadHabitat, HabitatTypeByBroadHabitat } from "../types";
+import { allHabitats } from "../habitats";
+import type { BroadHabitat, Habitat, HabitatTypeByBroadHabitat } from "../types";
 
 class OnSiteHabitat<
+    H extends Habitat,
     BH extends BroadHabitat
 > {
+    habitatData: H;
+
     constructor(
         public broadHabitat: BH,
         public habitatType: HabitatTypeByBroadHabitat<BH>,
-    ) { }
+        /* Area in hectares */
+        public area: number,
+    ) {
+        this.habitatData = allHabitats.find(h => h.level2Code === broadHabitat && h.type === habitatType) as H;
+    }
+
+    get isIrreplaceableHabitat() {
+        return this.habitatData.irreplaceable;
+    }
 }
 
