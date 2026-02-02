f5fae6cc3c65b84a1f19675b948a1b9d55b77e35
Matt Williams <matt@neverstew.com>
2026-02-02 13:21:24 +0000

    Publish for all different environments

--- index.ts
+++
@@ -1,7 +0,0 @@
-export { parseFile } from './src/parsers';
-export * as parsers from './src/parsers';
-
-export { headlineResults } from './src/headlineResults';
-export { tradingSummaries } from './src/tradingSummaries';
-
-export * as groupings from './src/groupings';
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { parseFile } from './parsers';
+export * as parsers from './parsers';
+
+export { headlineResults } from './headlineResults';
+export { tradingSummaries } from './tradingSummaries';
+
+export * as groupings from './groupings';
